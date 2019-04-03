76aa469549d73787a4e3e08da31c145275b879f2
sunzhongmou <sunzhongmou820@163.com>
2019-04-03 13:09:03 +0800

    Modified: make issue; Wayde

--- Makefile
+++ Makefile
@@ -3,11 +3,6 @@ fqdn ?= cicd.sunzhongmou.com
 unlock:
 	./unlock.sh
 
-update-submodule:
-	git submodule update --init --recursive \
-	&& cd lib/letsencrypt \
-	&& git pull origin master
-
 gen-pwd:
 	./scripts/1.gen-pwd.sh
 
@@ -17,9 +12,19 @@ issue:
 up:
 	docker-compose up -d
 
-gitcrypt:
+encrypt:
 	git-crypt init && \
 	git-crypt add-gpg-user me@sunwei.xyz
 
-letsencrypt:
-	cd lib && git submodule add -f git@github.com:sunwei/letsencrypt-www.git letsencrypt
+update-lib:
+	git submodule update --init --recursive \
+	&& cd lib/letsencrypt \
+	&& git pull origin master \
+	&& cd ../git-crypt \
+	&& git pull origin master
+
+init-lib:
+	cd lib && \
+	git submodule add -f git@github.com:sunwei/letsencrypt-www.git letsencrypt && \
+	git submodule add -f git@github.com:sunwei/git-crypt.git git-crypt
+
--- scripts/4.issue.sh
+++ scripts/4.issue.sh
@@ -2,24 +2,41 @@
 set -e
 
 BASEDIR="$(dirname "$0")"
+PROJDIR="$(cd ${BASEDIR}/../ && pwd)"
+CERTDIR="$(cd ${BASEDIR}/../cert && pwd)"
 WWW="$(cd ${BASEDIR}/../lib/letsencrypt && pwd)"
+GITCRYPT="$(cd ${BASEDIR}/../lib/git-crypt && pwd)"
 DOMAIN=
 
+_unlock_repo(){
+  make export-key keyid=me@sunwei.xyz
+  docker run --rm -it \
+   --env GPGKEY=me@sunwei.xyz.asc \
+   -v "${GITCRYPT}:/app/key" \
+   -v "${PROJDIR}:/app/repo" \
+   git-crypt \
+  sh -c "cd ./lib/letsencrypt && make unlock"
+}
+
 _issue_domain(){
-  cd ${BASEDIR}/../lib/letsencrypt && pwd
   ./www -p create "${DOMAIN}"
 }
 
 _transfer_cert(){
-  rm -rf /gocd-certs || true
-  mkdir /gocd-certs && cd /gocd-certs
-  cp "${WWW}/cert/${DOMAIN}-fullchain.csr" "./${DOMAIN}.crt"
-  cp "${WWW}/cert/${DOMAIN}-private-key.pem" "./${DOMAIN}.key"
+  cp "${WWW}/cert/${DOMAIN}-fullchain.csr" "${CERTDIR}/${DOMAIN}.crt"
+  cp "${WWW}/cert/${DOMAIN}-private-key.pem" "${CERTDIR}/${DOMAIN}.key"
 }
 
 main(){
   DOMAIN="${1}"
+
+  cd ${BASEDIR}/../lib/git-crypt && pwd
+  _unlock_repo
+  cd ..
+
+  cd ./letsencrypt
   _issue_domain
+
   _transfer_cert
 }
 
